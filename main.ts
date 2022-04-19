7fd18a46cb7365cd28de59e40197d81a066f4cda
pigplouf <103496137+pigplouf@users.noreply.github.com>
2022-04-19 11:16:02 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+let n = 0
+while (!(input.buttonIsPressed(Button.B))) {
+    if (input.buttonIsPressed(Button.A)) {
+        n += 1
+        if (n > 3) {
+            n = 1
+        }
+        if (n == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            if (n == 2) {
+                basic.showIcon(IconNames.Sad)
+            } else {
+                basic.showIcon(IconNames.Asleep)
+            }
+        }
+    }
+}
+basic.showString("Votre humeur")
+basic.showNumber(n)
